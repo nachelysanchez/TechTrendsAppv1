0d88798dab605024de44d8588fcbc8082a8658e8
Nachely Sánchez <nashely-5@hotmail.com>
2024-03-28 23:13:44 -0400

    Agregando contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechTrendsAppv1.Modelos;
+
+namespace TechTrendsAppv1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Publicaciones> Publicaciones { get; set; }
+        public DbSet<Comentarios> Comentarios { get; set; }
+        public DbSet<CalificacionPublicaciones> CalificacionPublicaciones { get; set; }
+        public DbSet<Permisos> Permisos { get; set; }
+        public DbSet<PermisosRoles> PermisosRoles { get; set; }
+        public DbSet<Etiquetas> Etiquetas { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<RespuestaComentarios> RespuestaComentarios { get; set; }
+        public DbSet<Estados> Estados { get; set; }
+        public DbSet<Plantillas> Plantillas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
