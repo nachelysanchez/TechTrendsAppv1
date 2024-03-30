c5bc4a901869781882bd4d919e906f207cb2337d
Nachely Sánchez <nashely-5@hotmail.com>
2024-03-29 22:09:12 -0400

    Culminando servicio de Roles

--- BLL/RolesServices/IRolesService.cs
+++ BLL/RolesServices/IRolesService.cs
@@ -1,9 +1,16 @@
-﻿using TechTrendsAppv1.Modelos;
+﻿using System.Linq.Expressions;
+using System.Threading;
+using TechTrendsAppv1.Modelos;
 
 namespace TechTrendsAppv1.BLL.RolesServices
 {
     public interface IRolesService
     {
-        Task<List<Modelos.Roles>> GetRolesAsync();
+        public Task<List<Modelos.Roles>> GetRolesAsync();
+        public Task<List<Modelos.Roles>> GetListAsync(Expression<Func<Modelos.Roles, bool>> criterio);
+        public Task<Modelos.Roles> GetRolAsync(int idRol);
+        public Task<bool> Guardar(Modelos.Roles rol);
+        public Task<bool> Eliminar(int id);
+
     }
 }
--- BLL/RolesServices/RolesBLL.cs
+++ BLL/RolesServices/RolesBLL.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 using System.Threading;
 using TechTrendsAppv1.BLL.RolesServices;
 using TechTrendsAppv1.DAL;
@@ -15,14 +16,146 @@ namespace TechTrendsAppv1.BLL.Roles
             this.contexto = _contexto;
         }
 
+        public bool Existe(int id)
+        {
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Roles.Any(t => t.IdRol == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
         /// <summary>
         /// Permite buscar una tarea mediante id
         /// </summary>
         /// <param name="id">El Id de la entidad que se desea buscar</param>
-        public async Task<List<Modelos.Roles>> GetRolesAsync()
+        async Task<List<Modelos.Roles>> IRolesService.GetRolesAsync()
         {
-            var roles = await contexto.Roles.ToListAsync();
-            return roles;
+            try
+            {
+                var roles = await contexto.Roles.ToListAsync();
+                return roles;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        async Task<List<Modelos.Roles>> IRolesService.GetListAsync(Expression<Func<Modelos.Roles, bool>> criterio)
+        {
+            List<Modelos.Roles> lista = new List<Modelos.Roles>();
+
+            try
+            {
+                lista = contexto.Roles.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        async Task<Modelos.Roles> IRolesService.GetRolAsync(int idRol)
+        {
+            Modelos.Roles rol = new();
+            try
+            {
+                rol = await contexto.Roles.FindAsync(idRol);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return rol;
+        }
+        async Task<bool> IRolesService.Guardar(Modelos.Roles rol)
+        {
+            if (!Existe(rol.IdRol))
+            {
+                return await Insertar(rol);
+            }
+            else
+            {
+                return await Modificar(rol);
+            }
+        }
+        private async Task<bool> Insertar(Modelos.Roles rol)
+        {
+            bool paso = false;
+            try
+            {
+                await contexto.Roles.AddAsync(rol);
+                paso = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        private async Task<bool> Modificar(Modelos.Roles rol)
+        {
+            bool paso = false;
+            try
+            {
+                contexto.Entry(rol).State = EntityState.Modified;
+                paso = await contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        async Task<bool> IRolesService.Eliminar(int id)
+        {
+            bool paso = false;
+            try
+            {
+                var rol = await contexto.Roles.FindAsync(id);
+                if (rol != null)
+                {
+                    contexto.Roles.Remove(rol);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return paso;
         }
     }
 }
