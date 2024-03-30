e3d8bb0415fdf9b53595386044885183f53269f2
Nachely Sánchez <nashely-5@hotmail.com>
2024-03-30 18:14:25 -0400

    Diseñando Login

---
+++ wwwroot/main.js
@@ -0,0 +1,10 @@
+﻿const passwordBtn = document.getElementById("password-eye");
+ 
+passwordBtn.addEventListener("click", (e) => {
+    const passwordInput = document.getElementById("password");
+    const icon = passwordBtn.querySelectorAll("i");
+
+    const isVisible = (icon[0].innerText == "visibility");
+    passwordInput.type = isVisible ? "password" : "text";
+    icon[0].innerText = isVisible ? "visibility_off" : "visibility";
+})
